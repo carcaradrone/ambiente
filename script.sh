755b927ad716b4dc733c3c17d294c3357bb30ebe
carcaradrone <60801676+carcaradrone@users.noreply.github.com>
2020-02-09 04:08:16 -0300

    script para configuração do ambiente

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+resp="s"
+
+echo "######### script do ambiente px4 #############"
+echo "Equipe carcará drones. UVA/RJ"
+sleep 1
+echo  .
+sleep 1
+echo  .
+sleep 1
+echo  .
+
+touch log.txt
+echo "inicia log.............." >> log.txt
+echo "atualizando repositorios..." >> log.txt
+apt update
+
+echo "##################"
+echo "##################"
+echo "##################"
+
+echo "atualizando pacotes..." >> log.txt
+apt -y upgrade
+
+echo "instalando pacotes essenciais..." >> log.txt
+apt -y install htop tree git net-tools rsync tcpdump netcat locate sudo terminator openssh-client openssh-server
+
+echo "##################"
+echo "##################"
+echo "##################"
+
+
+mkdir projetos
+cd projetos
+echo "clonando repositorio px4..." >> ../log.txt
+git clone https://github.com/PX4/Firmware.git
+echo "executando script do repo..." >> ../log.txt
+chmod a+x Firmware/Fools/setup/ubuntu.sh
+./Firmware/Tools/setup/ubuntu.sh
