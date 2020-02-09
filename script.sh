d005be95c82c3c14e6ce8ef4600ac29f98d2f264
ramonrrp <ramonrrp@gmail.com>
2020-02-09 04:18:43 -0300

    remove outro erro de caminho do clone

--- script.sh
+++ script.sh
@@ -2,6 +2,9 @@
 
 resp="s"
 
+mkdir ~/projetos
+cd ~/projetos
+
 echo "########## script do ambiente px4 ##############"
 echo "Equipe carcará drones. UVA/RJ"
 sleep 1
@@ -14,6 +17,7 @@ echo  .
 touch log.txt
 echo "inicia log.............." >> log.txt
 echo "atualizando repositorios..." >> log.txt
+
 apt update
 
 echo "##################"
@@ -21,18 +25,17 @@ echo "##################"
 echo "##################"
 
 echo "atualizando pacotes..."
+
 apt -y upgrade
 
-echo "instalando pacotes essenciais..." >> log.txt
+echo "instalando pacotes essenciais..." >> ./log.txt
+
 apt -y install htop tree git net-tools rsync tcpdump netcat locate sudo terminator openssh-client openssh-server
 
 echo "##################"
 echo "##################"
 echo "##################"
 
-
-mkdir ~/projetos
-cd ~/projetos
 echo "clonando repositorio px4..." >> ./log.txt
 git clone https://github.com/PX4/Firmware.git
 echo "executando script do repo..." >> ./log.txt
